a643e96a16d16866085ce1c5694fc2f9289de759
sjastrow <skjastrow@gmail.com>
2024-05-29 20:28:11 -0400

    added logic to method that accepts some content and adds it to database.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+ // Database and version
+  const jateDB = await openDB("jate", 1);
+  // New transaction specifying db and privileges
+  const tx = jateDB.transaction("jate", "readwrite");
+  // Open desired object store
+  const store = tx.objectStore("jate");
+  // Pass in content
+  const request = store.put({ id: 1, value: content });
+  // Confirmation
+  const result = await request;
+  console.log("data saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
